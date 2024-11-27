58581bd2a15bd68d2d9a737d247fec3797240559
naghim <mirtill@naghi.me>
2024-11-27 09:50:38 +0200

    Add problem 4 for formal

--- assets/py/evaluate_formal.py
+++ assets/py/evaluate_formal.py
@@ -1,9 +1,10 @@
-s='package'
-r='language'
-q='stdout'
-p='encoding'
-o='stdin'
-n='command'
+t='package'
+s='language'
+r='stdout'
+q='encoding'
+p='stdin'
+o='command'
+n='--stack'
 m='Case 3'
 l='--check'
 k='s0 s1 s2 s3\n0 1\ns0\ns2\ns0 0 s1\ns0 1 s3\ns1 0 s3\ns1 1 s2\ns2 0 s0\ns2 1 s2\ns3 0 s3\ns3 1 s0'
@@ -15,131 +16,135 @@ c='utf-8'
 b='\n'
 a='languageCommand'
 Z='--mini'
-Y='Case 2'
-X='Case 1'
-W=open
-T='setup'
-S=True
-R=None
-Q='--det'
-P='cases'
-M='root'
-K='arguments'
-H='testType'
-G=print
-F='output'
-E='inputType'
+Y=open
+V='setup'
+U=True
+T=None
+S='--det'
+R='Case 2'
+Q='Case 1'
+N='root'
+M='cases'
+J=print
+I='arguments'
+G='testType'
+E='output'
+D='inputType'
 B='input'
 A='name'
-from enum import Enum as U
-import tempfile as V,subprocess as L,multiprocessing as t,copy,sys as N,os as C
-class D(U):FILE=0;CONSOLE=1
-class I(U):TEST_EQUAL=0
-class O(U):PYTHON=0;CPP=1
-u='q0 q1 q2\n0 1\nq0\nq0\nq0 0 q2\nq0 1 q1\nq1 0 q2\nq1 1 q0\nq2 0 q1\nq2 1 q2'
-v='IGEN\nNEM\nIGEN\nIGEN\nNEM'
-w='q0 q1 q2\na b\nq0\nq1 q2\nq0 a q1\nq1 a q1\nq1 b q2\nq2 b q2'
-x='IGEN\nIGEN\nNEM\nNEM\nIGEN\nIGEN\nNEM\nNEM'
-y='q0 q1 q2\n0 1\nq0\nq2\nq0 0 q1\nq0 0 q2\nq0 1 q1\nq1 0 q2\nq1 1 q2'
-z='s0 s1 s2 s3\n0 1\ns0\ns1 s3\ns0 0 s1\ns0 1 s2\ns1 0 s3\ns1 1 s3\ns2 0 s3\ns2 1 s3'
-A0='q0 q1 q2\n0 1\nq0\nq2\nq0 0 q0\nq0 0 q1\nq0 1 q0\nq0 1 q1\nq1 0 q1\nq1 0 q2\nq1 1 q0\nq1 1 q1\nq1 1 q2\nq2 0 q1'
-A1='s0 s1 s2\n0 1\ns0\ns2\ns0 0 s1\ns0 1 s1\ns1 0 s2\ns1 1 s2\ns2 0 s2\ns2 1 s2'
-A2='q0 q1 q2 q3\n0 1\nq0\nq1 q3\nq0 0 q2\nq0 1 q1\nq1 0 q1\nq1 1 q2\nq1 1 q3\nq2 1 q2\nq3 0 q2\nq3 0 q3\nq3 1 q2'
-A3='s0 s1 s2 s3\n0 1\ns0\ns2 s3\ns0 0 s1\ns0 1 s2\ns1 1 s1\ns2 0 s2\ns2 1 s3\ns3 0 s3\ns3 1 s1'
-A4='q0 q1\n0 1\nq0\nq1\nq0 0 q0\nq0 1 q1\nq1 0 q0\nq1 1 q1'
-A5='s0 s1\n0 1\ns0\ns1\ns0 0 s0\ns0 1 s1\ns1 0 s0\ns1 1 s1'
-A6='q0 q1 q2 q3 q4 q5\n0 1\nq0\nq2\nq0 0 q1\nq0 1 q4\nq1 0 q4\nq1 1 q2\nq2 0 q0\nq2 1 q2\nq3 0 q5\nq3 1 q4\nq4 0 q4\nq4 1 q3\nq5 0 q4\nq5 1 q2'
-A7='s0 s1 s2 s4\n0 1\ns0\ns2\ns0 0 s1\ns0 1 s4\ns1 0 s4\ns1 1 s2\ns2 0 s0\ns2 1 s2\ns4 0 s4\ns4 1 s0'
-A8=k
-A9='q0 q1 q2 q3\n0 1\nq0\nq2\nq0 0 q1\nq0 1 q3\nq1 0 q3\nq1 1 q2\nq2 0 q0\nq2 1 q2\nq3 0 q3\nq3 1 q0'
-AA=k
-AB='q0 q1 q2\n0 1\nq0\nq0 q2\nq0 0 q1\nq0 1 q2\nq1 0 q1\nq1 1 q2\nq2 0 q1\nq2 1 q2'
-AC='s0 s1\n0 1\ns0\ns0\ns0 0 s1\ns0 1 s0\ns1 0 s1\ns1 1 s0'
-AD=[{A:'Problem 1 (DFA - Deterministic Finite Automaton)',P:[{A:X,E:D.FILE,H:I.TEST_EQUAL,B:u,F:v,K:[l,'10101,111,111110111010101,001,0021']},{A:Y,E:D.FILE,H:I.TEST_EQUAL,B:w,F:x,K:[l,'a,aa,abab,bbb,aaaaaaaaaaaab,aaaaabbbbb,aaaabbbbba,c']}]},{A:'Problem 2 (Transforming a Non-Deterministic Finite Automata Into a Deterministic One)',P:[{A:X,E:D.FILE,H:I.TEST_EQUAL,B:y,F:z,K:[Q]},{A:Y,E:D.FILE,H:I.TEST_EQUAL,B:A0,F:A1,K:[Q]},{A:m,E:D.FILE,H:I.TEST_EQUAL,B:A2,F:A3,K:[Q]},{A:'Case 4',E:D.FILE,H:I.TEST_EQUAL,B:A4,F:A5,K:[Q]}]},{A:'Problem 3 (Minimizing a Finite Automaton)',P:[{A:X,E:D.FILE,H:I.TEST_EQUAL,B:A6,F:[A7,A8],K:[Z]},{A:Y,E:D.FILE,H:I.TEST_EQUAL,B:A9,F:AA,K:[Z]},{A:m,E:D.FILE,H:I.TEST_EQUAL,B:AB,F:AC,K:[Z]}]}]
-class J(Exception):0
-def h():return C.path.join(V._get_default_tempdir(),next(V._get_candidate_names()))
-def AE(environment,case):
-	C=case;A=environment;G=copy.deepcopy(A[a]);H=C.get(K,[])
-	if C[E]==D.FILE:G.extend(['--input',A[B],'--output',A[F]])
-	if C[E]==D.CONSOLE:I=C[B]+b;J=c
-	else:I=R;J=R
+from enum import Enum as W
+import tempfile as X,subprocess as L,multiprocessing as u,copy,sys as O,os as F
+class C(W):FILE=0;CONSOLE=1
+class H(W):TEST_EQUAL=0
+class P(W):PYTHON=0;CPP=1
+v='q0 q1 q2\n0 1\nq0\nq0\nq0 0 q2\nq0 1 q1\nq1 0 q2\nq1 1 q0\nq2 0 q1\nq2 1 q2'
+w='IGEN\nNEM\nIGEN\nIGEN\nNEM'
+x='q0 q1 q2\na b\nq0\nq1 q2\nq0 a q1\nq1 a q1\nq1 b q2\nq2 b q2'
+y='IGEN\nIGEN\nNEM\nNEM\nIGEN\nIGEN\nNEM\nNEM'
+z='q0 q1 q2\n0 1\nq0\nq2\nq0 0 q1\nq0 0 q2\nq0 1 q1\nq1 0 q2\nq1 1 q2'
+A0='s0 s1 s2 s3\n0 1\ns0\ns1 s3\ns0 0 s1\ns0 1 s2\ns1 0 s3\ns1 1 s3\ns2 0 s3\ns2 1 s3'
+A1='q0 q1 q2\n0 1\nq0\nq2\nq0 0 q0\nq0 0 q1\nq0 1 q0\nq0 1 q1\nq1 0 q1\nq1 0 q2\nq1 1 q0\nq1 1 q1\nq1 1 q2\nq2 0 q1'
+A2='s0 s1 s2\n0 1\ns0\ns2\ns0 0 s1\ns0 1 s1\ns1 0 s2\ns1 1 s2\ns2 0 s2\ns2 1 s2'
+A3='q0 q1 q2 q3\n0 1\nq0\nq1 q3\nq0 0 q2\nq0 1 q1\nq1 0 q1\nq1 1 q2\nq1 1 q3\nq2 1 q2\nq3 0 q2\nq3 0 q3\nq3 1 q2'
+A4='s0 s1 s2 s3\n0 1\ns0\ns2 s3\ns0 0 s1\ns0 1 s2\ns1 1 s1\ns2 0 s2\ns2 1 s3\ns3 0 s3\ns3 1 s1'
+A5='q0 q1\n0 1\nq0\nq1\nq0 0 q0\nq0 1 q1\nq1 0 q0\nq1 1 q1'
+A6='s0 s1\n0 1\ns0\ns1\ns0 0 s0\ns0 1 s1\ns1 0 s0\ns1 1 s1'
+A7='q0 q1 q2 q3 q4 q5\n0 1\nq0\nq2\nq0 0 q1\nq0 1 q4\nq1 0 q4\nq1 1 q2\nq2 0 q0\nq2 1 q2\nq3 0 q5\nq3 1 q4\nq4 0 q4\nq4 1 q3\nq5 0 q4\nq5 1 q2'
+A8='s0 s1 s2 s4\n0 1\ns0\ns2\ns0 0 s1\ns0 1 s4\ns1 0 s4\ns1 1 s2\ns2 0 s0\ns2 1 s2\ns4 0 s4\ns4 1 s0'
+A9=k
+AA='q0 q1 q2 q3\n0 1\nq0\nq2\nq0 0 q1\nq0 1 q3\nq1 0 q3\nq1 1 q2\nq2 0 q0\nq2 1 q2\nq3 0 q3\nq3 1 q0'
+AB=k
+AC='q0 q1 q2\n0 1\nq0\nq0 q2\nq0 0 q1\nq0 1 q2\nq1 0 q1\nq1 1 q2\nq2 0 q1\nq2 1 q2'
+AD='s0 s1\n0 1\ns0\ns0\ns0 0 s1\ns0 1 s0\ns1 0 s1\ns1 1 s0'
+AE='q0 q1 q2\na b\nz0 z1\nq0\nz0\nq0\nq0 a z0 z0z1 q1\nq1 a z1 z1z1 q1\nq1 b z1 E q2\nq2 b z1 E q2\nq2 E z0 E q0'
+AF='IGEN\nNEM\nNEM\nNEM\nNEM\nNEM'
+AG='q0 q1 q2 q3\na b\nz0 z1\nq0\nz0\nq3\nq0 a z0 z0z1 q1\nq1 a z1 z1z1 q1\nq1 b z1 E q2\nq2 b z1 E q2\nq2 b z0 z0z0 q2\nq2 E z0 E q0'
+AH='IGEN\nIGEN\nNEM\nNEM\nNEM'
+AI=[{A:'Problem 1 (DFA - Deterministic Finite Automaton)',M:[{A:Q,D:C.FILE,G:H.TEST_EQUAL,B:v,E:w,I:[l,'10101,111,111110111010101,001,0021']},{A:R,D:C.FILE,G:H.TEST_EQUAL,B:x,E:y,I:[l,'a,aa,abab,bbb,aaaaaaaaaaaab,aaaaabbbbb,aaaabbbbba,c']}]},{A:'Problem 2 (Transforming a Non-Deterministic Finite Automata Into a Deterministic One)',M:[{A:Q,D:C.FILE,G:H.TEST_EQUAL,B:z,E:A0,I:[S]},{A:R,D:C.FILE,G:H.TEST_EQUAL,B:A1,E:A2,I:[S]},{A:m,D:C.FILE,G:H.TEST_EQUAL,B:A3,E:A4,I:[S]},{A:'Case 4',D:C.FILE,G:H.TEST_EQUAL,B:A5,E:A6,I:[S]}]},{A:'Problem 3 (Minimizing a Finite Automaton)',M:[{A:Q,D:C.FILE,G:H.TEST_EQUAL,B:A7,E:[A8,A9],I:[Z]},{A:R,D:C.FILE,G:H.TEST_EQUAL,B:AA,E:AB,I:[Z]},{A:m,D:C.FILE,G:H.TEST_EQUAL,B:AC,E:AD,I:[Z]}]},{A:'Problem 4 (Stack Automaton)',M:[{A:Q,D:C.FILE,G:H.TEST_EQUAL,B:AE,E:AF,I:[n,'aabb,abb,aab,abab,a,b']},{A:R,D:C.FILE,G:H.TEST_EQUAL,B:AG,E:AH,I:[n,'ab,abb,aaabb,a,b']}]}]
+class K(Exception):0
+def h():return F.path.join(X._get_default_tempdir(),next(X._get_candidate_names()))
+def AJ(environment,case):
+	F=case;A=environment;G=copy.deepcopy(A[a]);H=F.get(I,[])
+	if F[D]==C.FILE:G.extend(['--input',A[B],'--output',A[E]])
+	if F[D]==C.CONSOLE:J=F[B]+b;K=c
+	else:J=T;K=T
 	if H:G.extend(H)
-	A[n]=G;A[o]=I;A[p]=J
-def AF(environment,case):
-	A=environment;AE(A,case)
+	A[o]=G;A[p]=J;A[q]=K
+def AK(environment,case):
+	A=environment;AJ(A,case)
 	try:
-		B=L.run(A[n],cwd=A[d],capture_output=S,text=S,input=A[o],encoding=A[p])
+		B=L.run(A[o],cwd=A[d],capture_output=U,text=U,input=A[p],encoding=A[q])
 		for C in[B.stdout,B.stderr]:
 			if C:
 				C=C.strip()
-				if C:G(C.strip())
-		if B.returncode!=0:raise J(f"Program exited with status code {B.returncode}")
-		A[q]=B.stdout.strip()
-	except L.CalledProcessError as D:raise J(f"Couldn't run project: {D}")
-def AG(folder):
-	for(A,G,B)in C.walk(folder):
-		for D in B:
-			if D=='__main__.py':E=C.path.dirname(A);F=C.path.basename(A);return{r:O.PYTHON,M:E,s:F}
-def AH(folder):
-	for(A,E,B)in C.walk(folder):
-		for D in B:
-			if D=='CMakeLists.txt':return{r:O.CPP,M:A}
-def AI(folder):
-	for(B,G,D)in C.walk(folder):
-		for E in D:
-			A=C.path.join(B,E)
+				if C:J(C.strip())
+		if B.returncode!=0:raise K(f"Program exited with status code {B.returncode}")
+		A[r]=B.stdout.strip()
+	except L.CalledProcessError as D:raise K(f"Couldn't run project: {D}")
+def AL(folder):
+	for(A,G,B)in F.walk(folder):
+		for C in B:
+			if C=='__main__.py':D=F.path.dirname(A);E=F.path.basename(A);return{s:P.PYTHON,N:D,t:E}
+def AM(folder):
+	for(A,D,B)in F.walk(folder):
+		for C in B:
+			if C=='CMakeLists.txt':return{s:P.CPP,N:A}
+def AN(folder):
+	for(B,G,C)in F.walk(folder):
+		for D in C:
+			A=F.path.join(B,D)
 			try:
-				with W(A,'rb')as F:
-					if F.read(4)==b'\x7fELF':return A
+				with Y(A,'rb')as E:
+					if E.read(4)==b'\x7fELF':return A
 			except:pass
-def AJ(environment):A=environment;B=A[s];A[d]=A[M];A[a]=['python3','-m',B]
-def AK(environment):
-	B=environment;A=V.mkdtemp()
-	try:L.run(['cmake','-DCMAKE_BUILD_TYPE=Release',B[M]],cwd=A,check=S)
-	except L.CalledProcessError as D:raise J(f"Could not configure CMake project")
-	try:L.run(['make','-j',str(t.cpu_count())],cwd=A)
-	except L.CalledProcessError as D:raise J(f"Could not build CMake project")
-	C=AI(A)
-	if not C:raise J(f"Couldn't find built executable in CMake project")
+def AO(environment):A=environment;B=A[t];A[d]=A[N];A[a]=['python3','-m',B]
+def AP(environment):
+	B=environment;A=X.mkdtemp()
+	try:L.run(['cmake','-DCMAKE_BUILD_TYPE=Release',B[N]],cwd=A,check=U)
+	except L.CalledProcessError as D:raise K(f"Could not configure CMake project")
+	try:L.run(['make','-j',str(u.cpu_count())],cwd=A)
+	except L.CalledProcessError as D:raise K(f"Could not build CMake project")
+	C=AN(A)
+	if not C:raise K(f"Couldn't find built executable in CMake project")
 	B[d]=A;B[a]=[C]
-def AL(case,environment):
-	A=environment;C=h();D=h();A[B]=C;A[F]=D
-	with W(C,'w',encoding=c)as E:E.write(case[B])
-def AM(case,environment):
-	D=environment;E=D[B];A=D[F]
-	if not C.path.exists(A):raise J(f"Output file not created by program")
-	with W(A,'r',encoding=c)as G:I=G.read().strip()
-	if not j[case[H]](case,I):raise J(f"Output mismatch: expected different output in file")
-	if C.path.exists(E):C.remove(E)
-	if C.path.exists(A):C.remove(A)
-def AN(case,environment):
-	if not j[case[H]](case,environment[q]):raise J(f"Output mismatch: expected different output on stdout")
-def i(text):return b.join([A.strip()for A in text.split(b)])
-def AO(case,output):B=i(output.strip());A=case[F];C=[A]if isinstance(A,str)else A;return any([i(A.strip())==B for A in C])
-AP={O.PYTHON:{A:'Python',e:AG,f:AJ},O.CPP:{A:'CMake (C++)',e:AH,f:AK}}
-AQ={D.FILE:{T:AL,g:AM},D.CONSOLE:{T:R,g:AN}}
-j={I.TEST_EQUAL:AO}
-def AR():
-	O=C.getcwd();B=R
-	for(Q,F)in AP.items():
-		B=F[e](O)
+def AQ(case,environment):
+	A=environment;C=h();D=h();A[B]=C;A[E]=D
+	with Y(C,'w',encoding=c)as F:F.write(case[B])
+def AR(case,environment):
+	C=environment;D=C[B];A=C[E]
+	if not F.path.exists(A):raise K(f"Output file not created by program")
+	with Y(A,'r',encoding=c)as H:I=H.read().strip()
+	if not j[case[G]](case,I):raise K(f"Output mismatch: expected different output in file")
+	if F.path.exists(D):F.remove(D)
+	if F.path.exists(A):F.remove(A)
+def AS(case,environment):
+	if not j[case[G]](case,environment[r]):raise K(f"Output mismatch: expected different output on stdout")
+def i(text):return b.join([A.strip()for A in text.strip().split(b)])
+def AT(case,output):B=i(output);A=case[E];C=[A]if isinstance(A,str)else A;return any([i(A)==B for A in C])
+AU={P.PYTHON:{A:'Python',e:AL,f:AO},P.CPP:{A:'CMake (C++)',e:AM,f:AP}}
+AV={C.FILE:{V:AQ,g:AR},C.CONSOLE:{V:T,g:AS}}
+j={H.TEST_EQUAL:AT}
+def AW():
+	P=F.getcwd();B=T
+	for(Q,E)in AU.items():
+		B=E[e](P)
 		if B:break
-	if not B:G("❌ Couldn't find any project in the current folder ❌");N.exit(1)
-	G(f"Found {F[A]} project in {B[M]}")
-	try:F[f](B)
-	except J as H:G(f"❌ Error during build: {H}");N.exit(2)
-	G('Running tests...');K=False
-	for L in AD:
-		G(f"Problem: {L[A]}")
-		for D in L[P]:
-			G(f"Running case: {D[A]}")
+	if not B:J("❌ Couldn't find any project in the current folder ❌");O.exit(1)
+	J(f"Found {E[A]} project in {B[N]}")
+	try:E[f](B)
+	except K as G:J(f"❌ Error during build: {G}");O.exit(2)
+	J('Running tests...');I=False
+	for L in AI:
+		J(f"Problem: {L[A]}")
+		for C in L[M]:
+			J(f"Running case: {C[A]}")
 			try:
-				I=AQ[D[E]]
-				if I[T]:I[T](D,B)
-				AF(B,D)
-				if D[E]:I[g](D,B)
-			except J as H:G(f"❌ Failed {D[A]}: {H} ❌");K=S;continue
-			G(f"🎉 {D[A]} succeeded 🎉")
-	if K:G('❌ Some tests failed. ❌');N.exit(3)
-	G('🎉 All tests passed! Congratulations! 🎉');N.exit(0)
-if __name__=='__main__':AR()
+				H=AV[C[D]]
+				if H[V]:H[V](C,B)
+				AK(B,C)
+				if C[D]:H[g](C,B)
+			except K as G:J(f"❌ Failed {C[A]}: {G} ❌");I=U;continue
+			J(f"🎉 {C[A]} succeeded 🎉")
+	if I:J('❌ Some tests failed. ❌');O.exit(3)
+	J('🎉 All tests passed! Congratulations! 🎉');O.exit(0)
+if __name__=='__main__':AW()
